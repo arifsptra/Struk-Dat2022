b83ebdf2e8cb304c22ec87d93ff30b98e1772055
arifsptra <arif.sptrra@gmail.com>
2022-09-10 19:59:01 +0700

    write a program for the area of an isosceles triangle

---
+++ Praktikum/06-luas-segitiga-sama-kaki.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a, t;
+    cout << "Masukkan Alas: ";
+    cin >> a;
+    cout << "Masukkan Tinggi: ";
+    cin >> t;
+    float luas = 0.5*a*t;
+    cout << luas;
+    return 0;
+}
