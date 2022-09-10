b38c7665c4335ff10a7196829a341fcb73447217
arifsptra <arif.sptrra@gmail.com>
2022-09-10 19:55:37 +0700

    displaying Udinus writing n times

---
+++ Praktikum/05-cetak-udinus.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Ingin Mencetak Berapa Kali: ";
+    cin >> n;
+    for(int i=0; i<n; i++){
+        cout << "Udinus" << endl;
+    }
+    return 0;
+}
