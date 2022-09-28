0ad088672e2eb84003171029b6b7194fa1484f63
arifsptra <arif.sptrra@gmail.com>
2022-09-28 09:44:54 +0700

    make queue program

---
+++ Praktikum/24-queue-program.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+using namespace std;
+
+typedef struct antrian{
+    char name[30];
+    char sex;
+    int age;
+};
+
+antrian pasien[100];
+int n=1;
+
+inQueue(int x){
+    cout << "Input your name    : ";
+    cin >> pasien[x].name;
+    cout << "Input your sex     : ";
+    cin >> pasien[x].sex;
+    cout << "Input your age     : ";
+    cin >> pasien[x].age; 
+    n++;
+}
+
+maju(int x){
+    for(int i=x; i<=n; i++){
+        pasien[i] = pasien[i+1];
+    }
+}
+
+deQueue(int x){
+    cout << "Get Well Son: " << pasien[x].name << endl;
+    maju(x);
+    n--;
+}
+
+showQueue(){
+    for(int i=1; i<n; i++){
+        cout << i << " " << pasien[i].name << " " << pasien[i].sex << " " << pasien[i].age << endl;
+    }
+}
+
+int main(){
+    int choose;
+    do{
+        cout << "QUEUE IMPLEMENTATION" << endl;
+        cout << "1. INQUEUE" << endl;
+        cout << "2. DEQUEUE" << endl;
+        cout << "3. SHOWQUEUE" << endl;
+        cout << "9. EXIT" << endl;
+        cout << "Your choosen [1-9]: ";
+        cin >> choose;
+        if(choose==1){
+            inQueue(n);
+        }else if(choose==2){
+            int x;
+            cout << "Patient Number: ";
+            cin >> x;
+            deQueue(x);
+        }else if(choose==3){
+            showQueue();
+        }
+        cout << endl << endl;
+    }while(choose!=9);
+    return 0;
+}
